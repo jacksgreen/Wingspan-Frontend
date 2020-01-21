94ac422f05739334d196dbd67ac42d045d857729
gabegor <gabegor94@gmail.com>
2020-01-21 13:55:35 +0200

    updated to heroku

--- src/api.tsx
+++ src/api.tsx
@@ -2,6 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export async function getResponse() {
-    const getter = await axios.get('http://127.0.0.1:5000/')
+    const getter = await axios.get('https://itc-chrome-extension-server.herokuapp.com/')
     return getter.data
 }
