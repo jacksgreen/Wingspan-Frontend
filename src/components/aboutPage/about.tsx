cc284e6b967309be4bbfc9651baf8cb64a8d7d24
gabegor <gabegor94@gmail.com>
2020-01-22 15:52:41 +0200

    pulling

--- src/components/aboutPage/about.tsx
+++ src/components/aboutPage/about.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './style.css'
+import {
+    Card,
+    CardText,
+    CardBody,
+    CardTitle,
+} from 'reactstrap';
 
 const AboutPage: React.FC = () => {
     const [video, setVideo] = useState()
@@ -11,7 +17,19 @@ const AboutPage: React.FC = () => {
                 <source src="/forest.mp4" type="video/mp4" />
             </video>
             <div className="about-container">
-                
+                <Card style={{ width: '30rem' }}>
+                    <CardBody>
+                        <CardTitle><h1>WingSpan</h1></CardTitle>
+                        <CardText>
+                            Jack, write some beautiful poetry
+              </CardText>
+                        <CardText>
+                            Jack, Loves Alex,
+                            He loves her very much,
+                            She loves him even though he's a weirdo
+              </CardText>
+                    </CardBody>
+                </Card>
             </div>
         </div>
     )
