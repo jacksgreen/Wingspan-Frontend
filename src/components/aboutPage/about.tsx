4388ecfb3710c4b15290df7b0074e894ce0743f6
jackgreen <jack_s_green@me.com>
2020-01-23 00:13:02 +0200

    description page writing

--- src/components/aboutPage/about.tsx
+++ src/components/aboutPage/about.tsx
@@ -18,12 +18,27 @@ const AboutPage: React.FC = () => {
               <h1>WingSpan</h1>
             </CardTitle>
             <CardText>
-              Wingspan gives you the power to make more informed choices about
+              WingSpan gives you the power to make more informed choices about
               the products you buy. Choices that reflect your commitment to
               doing better by the planet, people, and animals. We believe that
               companies have a responsibility and should be transparent about
               their impact. How do the products you purchase get to you, and how
               do they compare to others?
+              <br />
+              <br />
+              By installing the chrome extension, WingSpan can quickly and
+              easily provide you with information about online products, so that
+              you can gain a better understanding of the impact that your
+              purchase will have on the environment.
+              <br />
+              <br />
+              Not only does WingSpan provide you with this information, but also
+              compares your chosen product with similar alternatives. Our
+              intelligent algorithm uses a variety of factors such as CO2
+              emssions to produce an 'eco ranking'. By comparing your product's
+              ranking to other available items, WingSpan makes it simple and
+              easy for customers to consider the environment in their purchasing
+              decisions.
             </CardText>
           </CardBody>
         </Card>
