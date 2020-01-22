012d27f99a0fe22ff1717e02762047e0d12af2cd
gabegor <gabegor94@gmail.com>
2020-01-22 10:51:00 +0200

    committing

--- src/api.tsx
+++ src/api.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export async function getResponse() {
-    const getter = await axios.get('https://itc-chrome-extension-server.herokuapp.com/')
+    // const getter = await axios.get('https://itc-chrome-extension-server.herokuapp.com/')
+    const getter = await axios.get('http://127.0.0.1:2700')
     return getter.data
 }
