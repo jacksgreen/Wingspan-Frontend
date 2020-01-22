5ab758e8bd4a9b16808c4320c8194df409d7d205
gabegor <gabegor94@gmail.com>
2020-01-22 12:08:08 +0200

    added all data

--- src/components/productPage/body.tsx
+++ src/components/productPage/body.tsx
@@ -37,22 +37,33 @@ const Body = () => {
 
     return (
         <div>
-
             {response['msg'] === 'Success' && loaded &&
                 <div className="align-items-center">
-                    <Card className="align-items-center p-1 w-50">
-                        <CardImg top className="mainCardImg" src={response.mainProduct.photoUrl} alt={response.mainProduct.photoUrl} />
-                        <CardBody>
-                            <CardTitle><h3>{response['mainProduct']['titleName']}</h3></CardTitle>
-                            <CardSubtitle><h5>{response['mainProduct']['subTitle']}</h5></CardSubtitle>
-                            <CardText>WingSpan Score: {response['mainProduct']['ecoScore']}</CardText>
-                            <CardText>Price: {response['mainProduct']['price']}</CardText>
-                            <CardText>Other Detials: {response['mainProduct']['details']}</CardText>
-                        </CardBody>
-                    </Card>
+                    <div className="d-flex justify-content-center">
+                        <Card className="align-items-center p-1 w-50 m-2">
+                            <CardImg top className="mainCardImg" src={response.mainProduct.photoUrl} alt={response.mainProduct.photoUrl} />
+                            <CardBody>
+                                <CardTitle><h3>{response['mainProduct']['titleName']}</h3></CardTitle>
+                                <CardSubtitle><h5>{response['mainProduct']['subTitle']}</h5></CardSubtitle>
+                                <CardText><b>WingSpan Score:</b> {response['mainProduct']['ecoScore']}</CardText>
+                                <CardText><b>Price:</b> {response['mainProduct']['price']}</CardText>
+                                <CardText><b>Other Details:</b> {response['mainProduct']['details']}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
                     <div>
                         <Table>
                             <thead>
+                                <tr>
+                                    {[response['mainProduct']['photoUrl'], response['firstSuggestion']['photoUrl'],
+                                    response['secondSuggestion']['photoUrl'], response['thirdSuggestion']['photoUrl']]
+                                        .map((content, index) =>
+                                            <td key={index}>
+                                                <div className="imgInTable"
+                                                    style={{ backgroundImage: `url(${content})` }}>
+                                                </div>
+                                            </td>)}
+                                </tr>
                                 <tr>
                                     {[response['mainProduct']['titleName'], response['firstSuggestion']['titleName'],
                                     response['secondSuggestion']['titleName'], response['thirdSuggestion']['titleName']]
@@ -62,22 +73,22 @@ const Body = () => {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                    {[response['mainProduct']['ecoScore'], response['firstSuggestion']['ecoScore'],
+                                    response['secondSuggestion']['ecoScore'], response['thirdSuggestion']['ecoScore']]
+                                        .map((content, index) =>
+                                            <td key={index}><b>WingSpan Score: </b>{content}</td>)}
                                 </tr>
                                 <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                    {[response['mainProduct']['price'], response['firstSuggestion']['price'],
+                                    response['secondSuggestion']['price'], response['thirdSuggestion']['price']]
+                                        .map((content, index) =>
+                                            <td key={index}><b>Price: </b>{content}</td>)}
                                 </tr>
                                 <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
+                                    {[response['mainProduct']['details'], response['firstSuggestion']['details'],
+                                    response['secondSuggestion']['details'], response['thirdSuggestion']['details']]
+                                        .map((content, index) =>
+                                            <td key={index}><b>More Details: </b>{content}</td>)}
                                 </tr>
                             </tbody>
                         </Table>
@@ -86,7 +97,7 @@ const Body = () => {
             {!loaded && <div>loading...</div>}
             {response['msg'] === 'No URL' && loaded && <div>Jacks Page</div>}
 
-        </div>
+        </div >
     )
 }
 
