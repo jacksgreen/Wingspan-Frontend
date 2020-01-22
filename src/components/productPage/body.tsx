e0cd96ba32eb5b9150de0aac8c1c199766e1a421
jackgreen <jack_s_green@me.com>
2020-01-22 11:19:53 +0200

    trying jacks page

--- src/components/productPage/body.tsx
+++ src/components/productPage/body.tsx
@@ -1,88 +1,112 @@
 import React, { useState, useEffect } from 'react';
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, Table
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Table
 } from 'reactstrap';
-import { getResponse } from '../../api'
-import './body.css'
-
+import { getResponse } from '../../api';
+import './body.css';
 
 interface Product {
-    price: string,
-    titleName: string,
-    subTitle: string,
-    ecoScore: string,
-    photoUrl: string,
-    details: string,
+  price: string;
+  titleName: string;
+  subTitle: string;
+  ecoScore: string;
+  photoUrl: string;
+  details: string;
 }
 interface Response {
-    mainProduct?: Product,
-    firstSuggestion?: Product,
-    secondSuggestion?: Product,
-    thirdSuggestion?: Product
+  msg?: string;
+  mainProduct?: Product;
+  firstSuggestion?: Product;
+  secondSuggestion?: Product;
+  thirdSuggestion?: Product;
 }
 
 const Body = () => {
-    const [response, setResponse] = useState<Response | undefined>()
-    const [loaded, setLoaded] = useState(false)
-    useEffect(() => {
-        getResponse().then(resp => {
-            console.log(resp)
-            setResponse(resp)
-            setLoaded(true)
-        }
-        )
-    }, [])
+  const [response, setResponse] = useState<Response | undefined>();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    getResponse().then(resp => {
+      console.log(resp);
+      setResponse(resp);
+      setLoaded(true);
+    });
+  }, []);
 
-    return (
-        <div>
-            {loaded &&
-                <div className="align-items-center">
-                    <Card className="align-items-center p-1 w-50">
-                        <CardImg top className="mainCardImg" src={response.mainProduct.photoUrl} alt={response.mainProduct.photoUrl} />
-                        <CardBody>
-                            <CardTitle><h3>{response['mainProduct']['titleName']}</h3></CardTitle>
-                            <CardSubtitle><h5>{response['mainProduct']['subTitle']}</h5></CardSubtitle>
-                            <CardText>WingSpan Score: {response['mainProduct']['ecoScore']}</CardText>
-                            <CardText>Price: {response['mainProduct']['price']}</CardText>
-                            <CardText>Other Detials: {response['mainProduct']['details']}</CardText>
-                        </CardBody>
-                    </Card>
-                    <div>
-                        <Table>
-                            <thead>
-                                <tr>
-                                    {[response['mainProduct']['titleName'], response['firstSuggestion']['titleName'],
-                                    response['secondSuggestion']['titleName'], response['thirdSuggestion']['titleName']]
-                                        .map((content, index) =>
-                                            <th key={index}>{content}</th>)}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </div></div>}
-            {!loaded && <div>loading...</div>}
+  return (
+    <div>
+      {response.msg === undefined && loaded && (
+        <div className='align-items-center'>
+          <Card className='align-items-center p-1 w-50'>
+            <CardImg
+              top
+              className='mainCardImg'
+              src={response.mainProduct.photoUrl}
+              alt={response.mainProduct.photoUrl}
+            />
+            <CardBody>
+              <CardTitle>
+                <h3>{response['mainProduct']['titleName']}</h3>
+              </CardTitle>
+              <CardSubtitle>
+                <h5>{response['mainProduct']['subTitle']}</h5>
+              </CardSubtitle>
+              <CardText>
+                WingSpan Score: {response['mainProduct']['ecoScore']}
+              </CardText>
+              <CardText>Price: {response['mainProduct']['price']}</CardText>
+              <CardText>
+                Other Detials: {response['mainProduct']['details']}
+              </CardText>
+            </CardBody>
+          </Card>
+          <div>
+            <Table>
+              <thead>
+                <tr>
+                  {[
+                    response['mainProduct']['titleName'],
+                    response['firstSuggestion']['titleName'],
+                    response['secondSuggestion']['titleName'],
+                    response['thirdSuggestion']['titleName']
+                  ].map((content, index) => (
+                    <th key={index}>{content}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope='row'>1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope='row'>2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope='row'>3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         </div>
-    )
-}
+      )}
+      {response.msg !== null && loaded && <div>jacks</div>}
+    </div>
+  );
+};
 
-export default Body
+export default Body;
