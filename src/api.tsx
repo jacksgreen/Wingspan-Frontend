18c17c0d99de6302e52053796fed65622e3400a8
Yinon Vahab <yinonv@gmail.com>
2020-01-22 11:06:42 +0200

    moved to local server

--- src/api.tsx
+++ src/api.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export async function getResponse() {
-    const getter = await axios.get('https://itc-chrome-extension-server.herokuapp.com/')
+    //const server = 'https://itc-chrome-extension-server.herokuapp.com/'
+    const server = 'http://127.0.0.1:2700/'
+    const getter = await axios.get(server)
     return getter.data
 }
